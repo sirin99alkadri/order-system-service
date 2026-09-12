3b910ccf51f787e40a2c92fca294fec8bc483e60
sirin99alkadri <sirinalkadri99@gmail.com>
2026-09-12 09:41:51 +0300

    initialize project

---
+++ src/main/java/com/example/OrderSystemService/OrderSystemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderSystemService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OrderSystemService/OrderSystemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderSystemService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
